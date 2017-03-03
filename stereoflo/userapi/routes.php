450cd3b82682b2b4d6e111514498fe07effea475
StereoFlo <StereoFlo@users.noreply.github.com>
2017-03-03 22:26:33 +0300

    change default GET route to any

--- stereoflo/userapi/routes.php
+++ stereoflo/userapi/routes.php
@@ -1,8 +1,7 @@
 <?php
 
 // get params. Used by cool hackers
-Route::get('/api/users/{act}', 'Stereoflo\Userapi\Controllers\FrontController@stub')->where('act', '[A-Za-z]+');
-
+Route::any('/api/users/{act}', 'Stereoflo\Userapi\Controllers\FrontController@stub')->where('act', '[A-Za-z]+');
 // post params
 Route::post('/api/users/login', 'Stereoflo\Userapi\Controllers\FrontController@login');
 Route::post('/api/users/logout', 'Stereoflo\Userapi\Controllers\FrontController@logout');
